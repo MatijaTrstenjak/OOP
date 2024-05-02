85804411362a7b683f10e99933e736d15fbfafff
Matija Trstenjak <84410644+MatijaTrstenjak@users.noreply.github.com>
2024-05-02 16:16:33 +0200

    Vjezba 7

--- Pitanje 1.cpp
+++ Pitanje 1.cpp
@@ -1,77 +1,147 @@
 ﻿/*
-Neka je zadana sljedeća funkcija "main":
+Potrebno je napraviti ispravne klase kako bi donji programski odsječak funkcionirao ispravno. Potrebno je modelirati nasljeđivanje,te osigurati da postoji apstraktna klasa u modelu (ispravno odabrati metodu koja je čista virtualna).
 
-int main() {
-    try {
-        test(2);
-        cout << "nastavak try-bloka...\n";
-    } catch (const PrevelikBroj& e) {
-        cout << e.poruka() << endl;
-    } catch (const PremaliBroj& e) {
-        cout << e.poruka() << endl;
-    } catch (...) {
-        cout << "... neka druga greska!";
-    }
+Dodatno, plaća za se računa na način da se osnovici plaće pribroje bonusi:
 
-    cout << "nastavak main funkcije\n";
+za igrača, svaki 10. gol donosi 10% osnovice kao bonus; dok svaka 5. asistencija donosi isto 10% osnovice kao bonus
+za trenera, svaka 5. pobjeda donosi bonus od 10% osnovice, svaki 10. remi (nerijeseno) donosi isto 10% osnovice, dok se za svaki 8. poraz skida 10% osnovice
+za detalje, pogledati primjere ispisa koda
+int main()
+{
+Igrac i("Luka", "Modric", 100000); //Ime, prezime, osnovica plaće
+i.dodajGolove(7);
+i.dodajAsistencije(12);
+i.ispis(); //Luka Modric - golova: 7, asistencija: 12
+cout << "Placa: " << i.izracunPlace() << endl; //Placa: 120000
+
+Igrac i2("C", "Ronaldo", 234505);
+i2.dodajGolove(57);
+i2.dodajAsistencije(32);
+i2.ispis(); //C Ronaldo - Golova : 57, Asistencije : 32.
+cout << "Placa: " << i2.izracunPlace() << endl; //Placa : 492461
+
+Trener t1("Hose", "Murinjo", 143009); //Ime, prezime, osnovica plaće
+t1.dodajPobjede(88); //Dodati broj pobjeda (W)
+t1.dodajRemi(12); //Dodati broj neriješenih utakmica (D)
+t1.dodajIzgubljene(0); //Dodati broj izgubljenih (L)
+t1.ispis(); //Hose Murinjo - Omjer (W/D/L): 88 / 12 / 0
+cout << "Placa: " << t1.izracunPlace() << endl; //Placa: 400425
+
+t1.dodajPobjede(11);
+t1.dodajIzgubljene(47);
+t1.ispis(); //Hose Murinjo - Omjer (W/D/L): 99 / 12 / 47
+cout << "Placa: " << t1.izracunPlace() << endl; //Placa: 357523
+
 }
 
-Nadopunite ovaj program tako da pokretanje funckije "main" daje sljedeći ispis:
-
-pocetak testa
-pocetak primjera
-prevelik broj
-nastavak main funkcije
-*/
-
-
-
-
+Demonstrirajte polimorfizam u funkciji main.*/
 #include <iostream>
 using namespace std;
 
-class PrevelikBroj
-{
-public:
-    const string poruka() const {
-        return "prevelik broj";
-    }
+ class Osoba {
+ protected:
+	 string ime;
+	 string prezime;
+	 int osnovica;
+
+ public:
+
+	 Osoba(string _ime, string _prezime, int _osnovica) {
+		 ime = _ime;
+		 prezime = _prezime;
+		 osnovica = _osnovica;
+	 }
+
+	 virtual void ispis() = 0;
+	 virtual int izracunPlace() = 0;
 };
-class PremaliBroj
+
+ class Igrac : public Osoba {
+ private:
+	 int brojGolova = 0;
+	 int brojAsistencija = 0;
+ public:
+	 Igrac(string _ime, string _prezime, int _osnovica) : Osoba(_ime, _prezime, _osnovica) {}
+
+	 void dodajGolove(int br) {
+		 brojGolova += br;
+	 }
+
+	 void dodajAsistencije(int br) {
+		 brojAsistencija += br;
+	 }
+
+	 void ispis() {
+		 cout << ime << " " << prezime << " - golova: " << brojGolova << ", asistencija: " << brojAsistencija << endl;
+	 }
+
+	 int izracunPlace() {
+		 int placa = osnovica;
+
+		 placa += osnovica * (brojGolova / 10) * 0.1;
+		 placa += osnovica * ((brojAsistencija / 5) * 0.1);
+
+		 return placa;
+	 }
+ };
+
+ class Trener : public Osoba {
+ private:
+	 int brojPobjeda = 0;
+	 int brojIzgubljenih = 0;
+	 int brojRemi = 0;
+ public:
+	 Trener(string _ime, string _prezime, int _osnovica) : Osoba(_ime, _prezime, _osnovica) {}
+
+	 void dodajPobjede(int br) {
+		 brojPobjeda += br;
+	 }
+	 void dodajIzgubljene(int br) {
+		 brojIzgubljenih += br;
+	 }
+	 void dodajRemi(int br) {
+		 brojRemi += br;
+	 }
+
+	 void ispis() {
+		 cout << ime << " " << prezime << " - Omjer (W/D/L): " << brojPobjeda << " / " << brojRemi << " / " << brojIzgubljenih << endl;
+	 }
+
+	 int izracunPlace() {
+		 int placa = osnovica;
+
+		 placa += osnovica * (brojPobjeda / 5) * 0.1;
+		 placa += osnovica * (brojRemi / 10) * 0.1;
+		 placa -= osnovica * (brojIzgubljenih / 8) * 0.1;
+
+		 return placa;
+	 }
+ };
+
+int main()
 {
-public:
-    const string poruka() const {
-        return "premali broj";
-    }
-};
+	Igrac i("Luka", "Modric", 100000); //Ime, prezime, osnovica plaće
+	i.dodajGolove(7);
+	i.dodajAsistencije(12);
+	i.ispis(); //Luka Modric - golova: 7, asistencija: 12
+	cout << "Placa: " << i.izracunPlace() << endl; //Placa: 120000
 
+	Igrac i2("C", "Ronaldo", 234505);
+	i2.dodajGolove(57);
+	i2.dodajAsistencije(32);
+	i2.ispis(); //C Ronaldo - Golova : 57, Asistencije : 32.
+	cout << "Placa: " << i2.izracunPlace() << endl; //Placa : 492460
 
-void test(int a) {
-    cout << "pocetak testa" << endl;
-    cout << "pocetak primjera" << endl;
+	Trener t1("Hose", "Murinjo", 143009); //Ime, prezime, osnovica plaće
+	t1.dodajPobjede(88); //Dodati broj pobjeda (W)
+	t1.dodajRemi(12); //Dodati broj neriješenih utakmica (D)
+	t1.dodajIzgubljene(0); //Dodati broj izgubljenih (L)
+	t1.ispis(); //Hose Murinjo - Omjer (W/D/L): 88 / 12 / 0
+	cout << "Placa: " << t1.izracunPlace() << endl; //Placa: 400424
 
-    if (a > 1)
-        throw PrevelikBroj();
-
-    else if (a < 1)
-        throw PremaliBroj();
+	t1.dodajPobjede(11);
+	t1.dodajIzgubljene(47);
+	t1.ispis(); //Hose Murinjo - Omjer (W/D/L): 99 / 12 / 47
+	cout << "Placa: " << t1.izracunPlace() << endl; //Placa: 357521
 }
 
-
-int main() {
-    try {
-        test(1);
-        cout << "nastavak try-bloka...\n";
-    }
-    catch (const PrevelikBroj& e) {
-        cout << e.poruka() << endl;
-    }
-    catch (const PremaliBroj& e) {
-        cout << e.poruka() << endl;
-    }
-    catch (...) {
-        cout << "... neka druga greska!";
-    }
-
-    cout << "nastavak main funkcije\n";
-}
--- Pitanje 2.cpp
+++ Pitanje 2.cpp
@@ -1,101 +1,94 @@
-/*
-Definirajte klasu Tekst koja predstavlja tekstualni tip podatka (kao string), s
-tim da podrzava operacije *, - i +:
-- operacija * umnozava tekst. Na primjer, ako je x = "abc" onda x * 3 daje novi
-objekt tipa Tekst koji sadrzi tekst "abcabcabc". Ova operacija mora raditi i
-kada je broj s lijeve strane, to jest x * 3 treba davati isti rezultat kao i
-3 * x
-- operacija - uklanja zadani znak iz teksta. Na primjer, ako je x = "uvala"
-onda x - 'a' daje novi objekt tipa Tekst koji sadrzi tekst "uvl", to jest
-bez slova 'a'
-- operacija + spaja dva teksta u jedan novi tekst. Na primjer, ako je x = "ab"
-i y = "cd", onda x + y daje novi objekt tipa Tekst koji sadrzi tekst "abcd"
-- treba redefinirati i operator << da radi s objektima tipa Tekst
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
 
-Ovu klasu treba definirati tako da funkcija main radi kako je ispod prikazano.
-*/
-
-#define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
 using namespace std;
 
-class Tekst {
+class Proizvod {
+protected:
+    string naziv;
+    double cijena;
+
 public:
-    char* tekst;
+    Proizvod(string _naziv, double _cijena) : naziv(_naziv), cijena(_cijena) {}
 
-    Tekst() = default;
+    virtual void print() const = 0;
 
-    Tekst(const char* s) {
-        tekst = new char[strlen(s) + 1];
-        strcpy(tekst, s);
+    virtual double getKoefKvalitete() const = 0;
 
-    };
-    ~Tekst() {
-        tekst = nullptr;
+    double getCijena() const {
+        return cijena;
     }
-
-    Tekst operator*(int br) const {
-        char* result = new char[strlen(tekst) * br + 1];
-        strcpy(result, tekst);
-        for (int i = 1; i < br; ++i) {
-            strcat(result, tekst);
-        }
-
-        return Tekst(result);
-    }
-
-    Tekst operator +(Tekst t) {
-        char* result = new char[strlen(tekst) + strlen(t.tekst)];
-        strcpy(result, tekst);
-        strcat(result, t.tekst);
-
-        return Tekst(result);
-    }
-
-    Tekst operator -(char a) {
-        char* result = new char[strlen(tekst)];
-        int brojac = 0;
-        for (int i = 0; i < strlen(tekst); i++) {
-            if (tekst[i] != a)
-                result[brojac++] = tekst[i];
-        }
-
-        result[brojac] = '\0';
-
-        return Tekst(result);
-    }
-
 };
 
-Tekst operator*(int br, const Tekst& t) {
-    return t * br;
+class LegoIgracka : public Proizvod {
+private:
+    int brKomada;
+
+public:
+    LegoIgracka(string _naziv, double _cijena, int _brKomada) : Proizvod(_naziv, _cijena), brKomada(_brKomada) {}
+
+    void print() const override {
+        cout << "Lego igracka: " << naziv << ", Cijena: " << cijena << ", Broj komada: " << brKomada << endl;
+    }
+
+    double getKoefKvalitete() const override {
+        return brKomada / cijena;
+    }
+};
+
+class Automobil : public Proizvod {
+private:
+    int godProizvodnje;
+    double maxBrzina;
+
+public:
+    Automobil(string _naziv, double _cijena, int _godProizvodnje, double _maxBrzina) : Proizvod(_naziv, _cijena), godProizvodnje(_godProizvodnje), maxBrzina(_maxBrzina) {}
+
+    void print() const override {
+        cout << "Automobil: " << naziv << ", Cijena: " << cijena << ", Godina proizvodnje: " << godProizvodnje
+            << ", Maksimalna brzina: " << maxBrzina << endl;
+    }
+
+    double getKoefKvalitete() const override {
+        return 1.0 / (2024 - godProizvodnje) * maxBrzina / 100.0 * cijena / 100000.0;
+    }
+};
+
+int main() {
+    vector<Proizvod*> vp = {
+            new LegoIgracka("Star Wars - Millenium Falcon", 2250.99, 756),
+            new Automobil("BMW", 51230, 2013, 207.34),
+            new LegoIgracka("Vlakic na upravljanje", 1750.99, 877),
+            new Automobil("Yugo 45", 14500, 1988, 127.88),
+    };
+
+    for (int i = 0; i < vp.size(); i++) {
+        vp[i]->print();
+    }
+
+    double maxKoef = 0.0;
+    Proizvod* najkvalitetnijiProizvod = nullptr;
+    for (int i = 0; i < vp.size(); i++) {
+        if (vp[i]->getKoefKvalitete() > maxKoef) {
+            maxKoef = vp[i]->getKoefKvalitete();
+            najkvalitetnijiProizvod = vp[i];
+        }
+    }
+
+    if (najkvalitetnijiProizvod != nullptr) {
+        cout << "\nProizvod s najvecim koeficijentom kvalitete: ";
+        najkvalitetnijiProizvod->print();
+        cout << "Koeficijent kvalitete: " << maxKoef << endl;
+    }
+    else {
+        cout << "\nNema dostupnih proizvoda." << endl;
+    }
+
+    for (int i = 0; i < vp.size(); i++) {
+        delete vp[i];
+    }
+
+    return 0;
 }
-
-ostream& operator <<(ostream& izlaz, Tekst t) {
-
-    for(int i = 0; i < strlen(t.tekst); i++)
-        izlaz << t.tekst[i];
-
-    return izlaz;
-}
-
-int main()
-{
-    Tekst t = "abc";
-    cout << t.tekst << endl;
-
-    Tekst p = t * 3;                // "umnozavanje" teksta
-    cout << p << endl;                // ispisuje abcabcabc
-    cout << 2 * t << endl;            // ispisuje abcabc
-
-    // t mora ostati nepromijenjen!!!
-    cout << t << endl;                // ispisuje abc
-
-    cout << t + p << endl;            // ispisuje abcabcabcabc
-    cout << t + p - 'a' << endl;    // ispisuje bcbcbcbc
-
-    //// p i t moraju ostati nepromijenjeni!!!
-    cout << p << endl;                // ispisuje abcabcabc
-    cout << t << endl;                // ispisuje abc
-}
-
